c2a885f50788e134334d6b7f8764ff7a459828f9
umarfarooqdev789 <umarfarooqdev789@gmail.com>
2026-03-14 14:39:57 +0500

    delet App and add Layout file

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,14 +1,12 @@
 function Login() {
     return (
         <section id="section-seven" className="scroll-animate">
-            <div className="w-full bg-black flex justify-center px-[100px] py-[50px]">
-
-                {/* FORM ONLY - NO IMAGE */}
+            <div className="w-full bg-black flex justify-center px-25 py-12.5">
                 <div className="w-full max-w-xl bg-[#F8F9FA] flex justify-center items-center p-6 rounded-lg">
                     <form
                         target="form-target"
                         id="contact-form"
-                        action="https://formspree.io/f/xyznlwoq"
+                        action=""
                         method="POST"
                         className="w-full flex flex-col gap-4 p-10"
                     >
@@ -41,16 +39,6 @@ function Login() {
                         />
 
                         <p className="msg3 text-red-500 text-sm"></p>
-
-                        <textarea
-                            name="textarea"
-                            id="txtarea"
-                            placeholder="Message"
-                            rows="6"
-                            className="w-full p-3 text-sm border border-gray-300 rounded-md resize-none focus:border-black"
-                        ></textarea>
-
-                        <p className="msg4 text-red-500 text-sm"></p>
 
                         <button className="w-2/5 py-4 font-medium bg-[#E6A730] rounded-full hover:bg-[#FFBD39] transition">
                             SEND MESSAGE
